e6328ce1be8f99979fcd67e631c3b8180e0e3a0f
Timbusien <timbusiennavi@gmail.com>
2023-09-17 16:32:06 +0500

    update 2

--- buttons.py
+++ buttons.py
@@ -1,4 +1,12 @@
 from telebot import types
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+def languages():
+    buttons = InlineKeyboardMarkup(row_width=2)
+    rus = InlineKeyboardButton(text='Русский язык', callback_data='rus')
+    uzb = InlineKeyboardButton(text="O'zbek tili", callback_data='uzb')
+    buttons.row(rus, uzb)
+    return buttons
 
 def choice():
     buttons = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -13,6 +21,24 @@ def get_number():
     back = types.KeyboardButton('далее')
     buttons.add(number, back)
     return buttons
+
+
+
+def choice_uzb():
+    buttons = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    reg = types.KeyboardButton('Toldirish')
+    #help_bar = types.KeyboardButton('Помощь')
+    buttons.add(reg)
+    return buttons
+
+def get_number_uzb():
+    buttons = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    number = types.KeyboardButton('nomerni yuklash', request_contact=True)
+    back = types.KeyboardButton('bosqa joyga')
+    buttons.add(number, back)
+    return buttons
+
+
 
 
 
